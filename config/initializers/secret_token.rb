636bd56188b777f6738b9fd177750c2057c74617
raviteja <adusumalliraviteja@gmail.com>
2012-10-27 15:08:58 +0530

    this is first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IamgeUpload::Application.config.secret_token = '3b0cefe5f62a50fb13142169e1422c44a52231a793aa7e8bf0f9526bb4e15024be7dcd9777b483b31f995f92ab8e1ead8696f7535d4f0004594ce9d5e29da794'
